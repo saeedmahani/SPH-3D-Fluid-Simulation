f1d65906df823d1bc71c37f77e28759ec8569121
Saeed Mahani <smahani@nevion.com>
2012-02-24 15:48:18 -0800

    Initial Commit

---
+++ 594G Final Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  594G Final Project
+//
+//  Created by Saeed Mahani on 2/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
